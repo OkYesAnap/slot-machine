e1695caac342069b26dbf9b6a2887340b8eb1f3c
Panaseiko Andrii <andrii.panaseiko@akvelon.com>
2024-09-18 14:19:37 +0300

    hot fix

--- src/components/SlotMachine/TextMessage.tsx
+++ src/components/SlotMachine/TextMessage.tsx
@@ -1,6 +1,6 @@
 import {Text} from "@pixi/react";
 import {CANVAS_HEIGHT, CANVAS_WIDTH, WIN_MULTIPLIER} from "../../constants/slotMachineConstants";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {TextStyle} from "pixi.js";
 import {IRollData} from "../../types/slotMachiteTypes";
 
@@ -17,10 +17,20 @@ const TextMessage: React.FC<{ showText: boolean, betControllerData: IRollData }>
 	                                                                                       showText,
 	                                                                                       betControllerData
                                                                                        }) => {
+	const [show, setShow] = useState(showText);
+	useEffect(() => {
+		let timeout: NodeJS.Timeout
+		if (showText) {
+			timeout = setTimeout(() => {
+				setShow(showText);
+			}, 100);
+		} else setShow (showText);
+		return () => clearTimeout(timeout)
+	}, [showText])
 	const {winLines, last_bet, win, balance} = betControllerData
 	return (
 		<>
-			{showText && (
+			{show && (
 				<>
 					{balance <= 0 ? (
 						<Text
